9148b568cd6e4506e00404afeab4253db1d86dd1
THAYNÁ DE OLIVEIRA FREIRE <24122484@CFP127ALUNO.LOCAL>
2025-02-18 11:34:03 -0300

    teste

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const botaoTrocarImagem = document.getElementById('trocar-imagem')
+
+const imagens = [
+    'url("./img/ceu-dia.jpg")',
+    'url("./img/ceu-noite.jpg")',
+    'url("./img/ceu-tarde.jpg")'
+]
+
+let indiceAtual = 0
+
+function trocarImagem() {
+    indiceAtual = (indiceAtual + 1) % imagens.length
+    document.documentElement.style.setProperty('--imagem-fundo', imagens[indiceAtual])
+}
+
+botaoTrocarImagem.addEventListener('click', trocarImagem)
